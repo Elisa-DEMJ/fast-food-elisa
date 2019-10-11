2af81367ce4eccf742ea420c86e6ef8f12f58dbb
Elisa D <dalleauelisa@gmail.com>
2019-10-11 16:40:41 +0200

    ajoute du fichier index

---
+++ index.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<title>Hello, world!</title>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">FAST FOOD</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Menu</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link" href="#">
+						Conatct
+					</a>
+
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Test</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		</div>
+	</nav>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10">
+				<h2>Welcome :</h2>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+						</div>
+
+						<div class="col-sm">
+							<div class="my-2 my-sm-0" style="width: 18rem;">
+								<img src="https://dummyimage.com/800x600/616161/fff.jpg" class="card-img-top" alt="...">
+								<p>One of three columns
+									<button type="button" class="btn btn-primary btn-lg btn-block">Primary</button>
+							</div>
+
+						</div>
+					</div>
+
+
+				</div>
+				<div class="col-sm-2">
+					<h2>Commande :</h2>
+					<br>
+					<div class="card" style="width: 20rem;">
+						<div class="card-body">
+							<h5 class="card-title">Special title treatment</h5>
+							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+							<button class="btn btn-success my-2 my-sm-0" type="submit">Commander</button>
+							<br>
+
+							<button class="btn btn-danger my-2 my-sm-1" type="submit">Suprimer la commande</button>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+
+	</div>
+
+
+
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
